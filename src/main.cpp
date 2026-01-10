9446c86f389e5a96a9072af40e631f0fd137f0bc
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2026-01-10 02:01:34 +0100

    feat: rewrite application setup code to use RAII and SDL renderer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,166 +1,109 @@
 #include <imgui.h>
 #include <imgui_impl_sdl3.h>
-#include <imgui_impl_sdlgpu3.h>
+#include <imgui_impl_sdlrenderer3.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <string>
 
-constexpr int screen_width { 960 };
-constexpr int screen_height { 540 };
+// Enumeration of possible status values for the application.
+enum ApplicationStatus {
+    SUCCESS,                // Success.
+    INITIALIZATION_ERROR    // An error occurred during setup, e.g. when creating a window. (SDL errors.)
+};
+
+// Because RAII is pretty nice <3
+class Application {
+private:
+    ApplicationStatus status;
+    int window_width;
+    int window_height;
+    std::string window_title;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+public:
+    /*
+    Get the status of the application.
+
+    @return The status of the application as an `ApplicationStatus` value.
+    */
+    int get_status() {
+        return status;
+    }
+
+    /*
+    Initialise the application with the provided window dimensions and title.
+
+    @return An `Application` object. Make sure to call `get_status()` on the returned object before 
+    using it to find out if initialisation has failed!
+    */
+    Application(int window_width, int window_height, std::string window_title)
+    : window_width(window_width), window_height(window_height), window_title(window_title) {
+        // Set exit status to 0 (success).
+        status = SUCCESS;
+
+        // Initialize SDL.
+        if (SDL_Init(SDL_INIT_VIDEO) == false) {
+            status = INITIALIZATION_ERROR;
+            return;
+        }
+
+        // Create an SDL window.
+        float scale;
+        if ((scale = scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay())) == 0) {
+            scale = 1;  // Use the scaling factor expected by the display based on its DPI settings, default to 1.
+        }
+        SDL_Window* window = SDL_CreateWindow(window_title.c_str(), (int)(window_width * scale), (int)(window_height * scale), SDL_WINDOW_RESIZABLE);
+        if (window == nullptr) {
+            status = INITIALIZATION_ERROR;
+            return;
+        }
+
+        // Create an SDL renderer.
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+        if (renderer == nullptr) {
+            status = INITIALIZATION_ERROR;
+            return;
+        }
+    }
+
+    ~Application() {
+        /*
+        SDL cleanup.
+
+        If the application is not properly initialised, e.g. due to an error,
+        some fields may be null and that case must be handled properly.
+
+        There might be a cleaner way to do this, but we are not really initialising
+        that much stuff so it probably does not matter.
+        */
+        if (renderer != nullptr) {
+            SDL_DestroyRenderer(renderer);
+        }
+        if (window != nullptr) {
+            SDL_DestroyWindow(window);
+        }
+        SDL_Quit();
+
+        SDL_Log("Finished cleaning up application.");
+    }
+};
 
 int main() {
+    Application application = Application(960, 540, "I am a window :3");
 
-    // Initialise an SDL window and two surfaces.
-    SDL_Window* window { nullptr };
-    SDL_Surface* surface { nullptr };
-    SDL_Surface* bitmap_surface { nullptr };
-
-    // Initialise SDL.
-    if (SDL_Init(SDL_INIT_VIDEO) == false) {
-        SDL_Log("SDL initialisation: %s\n", SDL_GetError());
-        return 1;
+    // Check the application's status before exit.
+    int status = application.get_status();
+    switch (status) {
+        case SUCCESS:
+            SDL_Log("Application successfully terminated.");
+            break;
+        case INITIALIZATION_ERROR:
+            SDL_Log("Initialization error: %s\n", SDL_GetError());
+            break;
+        default:
+            SDL_Log("Undefined application status: %d\n", status);
     }
 
-    // Get window and surface.
-    window = SDL_CreateWindow("I am a window :3", screen_width, screen_height, 0);
-    if (window == nullptr) {
-        SDL_Log("SDL window: %s\n", SDL_GetError());
-        return 2;
-    } else {
-        surface = SDL_GetWindowSurface(window);
-        if (surface == nullptr) {
-            SDL_Log("SDL get window surface: %s\n", SDL_GetError());
-            return 5;
-        }
-    }
-
-    // Load /assets/uwu.bmp.
-    std::string image_path { "assets/uwu.bmp" };
-    bitmap_surface = SDL_LoadBMP(image_path.c_str());
-    if (bitmap_surface == nullptr) {
-        SDL_Log("SDL load bitmap: %s\n", SDL_GetError());
-        return 3;
-    }
-
-    // Initialise GPU device using SDL to make it cross-platform.
-    SDL_GPUDevice* gpu_device = SDL_CreateGPUDevice(
-        SDL_GPU_SHADERFORMAT_DXIL | SDL_GPU_SHADERFORMAT_MSL | SDL_GPU_SHADERFORMAT_SPIRV,
-        true,   // TODO: Only enable this in debug builds.
-        nullptr
-    );
-    if (gpu_device == nullptr) {
-        SDL_Log("SDL initialise GPU: %s\n", SDL_GetError());
-        return 4;
-    }
-
-    // Claim window for GPU device.
-    if (SDL_ClaimWindowForGPUDevice(gpu_device, window) == false) {
-        SDL_Log("SDL claim GPU for window: %s\n", SDL_GetError());
-        return 6;
-    }
-
-    // Set-up ImGUI context.
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-
-    // Configure I/O to enable keyboard navigation.
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-
-    // Set-up rendering backend.
-    ImGui_ImplSDL3_InitForSDLGPU(window);
-    ImGui_ImplSDLGPU3_InitInfo sdlgpu3_init_info {};
-    sdlgpu3_init_info.Device = gpu_device;
-    sdlgpu3_init_info.ColorTargetFormat = SDL_GetGPUSwapchainTextureFormat(gpu_device, window);
-    sdlgpu3_init_info.MSAASamples = SDL_GPU_SAMPLECOUNT_1;
-    sdlgpu3_init_info.SwapchainComposition = SDL_GPU_SWAPCHAINCOMPOSITION_SDR;
-    sdlgpu3_init_info.PresentMode = SDL_GPU_PRESENTMODE_VSYNC;
-    ImGui_ImplSDLGPU3_Init(&sdlgpu3_init_info);
-
-    // Simple main loop to keep the window open.
-    bool quit {false};
-    SDL_Event event;
-    SDL_zero(event);
-    while (quit == false) {
-        // Poll for events.
-        while (SDL_PollEvent(&event) == true) {
-            // Forward events to the backend.
-            ImGui_ImplSDL3_ProcessEvent(&event);
-            if (event.type == SDL_EVENT_QUIT) {
-                quit = true;
-            }
-        }
-        
-        // Render bitmap :3
-        // SDL_BlitSurface(bitmap_surface, nullptr, surface, nullptr);
-        // SDL_UpdateWindowSurface(window);
-
-        // Start the Dear ImGUI frame.
-        ImGui_ImplSDLGPU3_NewFrame();
-        ImGui_ImplSDL3_NewFrame();
-        ImGui::NewFrame();
-
-        // Show ImGUI demo window.
-        ImGui::ShowDemoWindow();
-
-        // Start of rendering the ImGUI frame.
-        ImGui::Render();
-
-        // Set-up command buffer and texture for drawing.
-        SDL_GPUCommandBuffer* command_buffer = SDL_AcquireGPUCommandBuffer(gpu_device);
-        if (command_buffer == nullptr) {
-            SDL_Log("SDL get GPU command buffer: %s\n", SDL_GetError());
-            continue;
-        }
-        SDL_GPUTexture* swapchain_texture;
-        bool swapchain_texture_acquired;
-        swapchain_texture_acquired = SDL_WaitAndAcquireGPUSwapchainTexture(command_buffer, window, &swapchain_texture, nullptr, nullptr);
-        if (swapchain_texture_acquired == false || swapchain_texture == nullptr) {
-            SDL_Log("SDL get acquire swapchain texture: %s\n", SDL_GetError());
-            continue;
-        }
-
-        // Prepare draw data.
-        ImDrawData* draw_data { ImGui::GetDrawData() };
-        ImGui_ImplSDLGPU3_PrepareDrawData(draw_data, command_buffer);
-
-        // Set-up and start a render pass.
-        SDL_GPUColorTargetInfo color_target_info = {};
-        color_target_info.texture = swapchain_texture;
-        color_target_info.clear_color = SDL_FColor { 0.0, 0.0, 0.0, 0.0 };
-        color_target_info.load_op = SDL_GPU_LOADOP_CLEAR;
-        color_target_info.store_op = SDL_GPU_STOREOP_STORE;
-        color_target_info.mip_level = 0;
-        color_target_info.layer_or_depth_plane = 0;
-        color_target_info.cycle = false;
-        SDL_GPURenderPass* render_pass = SDL_BeginGPURenderPass(command_buffer, &color_target_info, 1, nullptr);
-
-        // Render GUI.
-        ImGui_ImplSDLGPU3_RenderDrawData(draw_data, command_buffer, render_pass);
-
-        // End of rendering the ImGUI frame.
-        SDL_EndGPURenderPass(render_pass);
-
-        // Submit command buffer.
-        SDL_SubmitGPUCommandBuffer(command_buffer);
-    }
-
-    // Shutdown ImGUI.
-    ImGui_ImplSDL3_Shutdown();
-    ImGui_ImplSDLGPU3_Shutdown();
-    ImGui::DestroyContext();
-
-    // Destroy SDL window and surfaces.
-    SDL_WaitForGPUIdle(gpu_device);
-    SDL_DestroySurface(bitmap_surface);
-    bitmap_surface = nullptr;
-    
-    SDL_DestroyWindow(window);
-    window = nullptr;
-    surface = nullptr;
-
-    // Quit.
-    SDL_Quit();
-
-    return 0;
+    return status;
 }
