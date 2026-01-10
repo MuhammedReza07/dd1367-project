4a322ba3731ae2f82d8ba5d6f4e76892c13f5da2
Muhammed Reza Mehdi Hamid Muhammed (Linux Mint on IdeaPad 1i) <muhammed.reza.mehdi@gmail.com>
2026-01-10 03:28:02 +0100

    feat: added UwU with window resizing enabled. Also resolved some bugs in the initialisation code.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,8 @@
 // Enumeration of possible status values for the application.
 enum ApplicationStatus {
     SUCCESS,                // Success.
-    INITIALIZATION_ERROR    // An error occurred during setup, e.g. when creating a window. (SDL errors.)
+    INITIALIZATION_ERROR,   // An error occurred during setup, e.g. when creating a window. (SDL errors.)
+    RUNTIME_ERROR           // An error occurred during runtime, e.g. when loading a file.
 };
 
 // Because RAII is pretty nice <3
@@ -23,12 +24,32 @@ private:
 
 public:
     /*
-    Get the status of the application.
+    Print a human-readable message reflecting the current status of the application.
 
-    @return The status of the application as an `ApplicationStatus` value.
+    @return `true` if `status` is `SUCCESS`, `false` otherwise.
     */
-    int get_status() {
-        return status;
+    bool print_status() {
+        // Get error messages. (e.g. from SDL.)
+        std::string sdl_error = SDL_GetError();
+
+        switch (status) {
+            case SUCCESS:
+                return true;
+            case INITIALIZATION_ERROR:
+                SDL_Log("Initialization error: %s\n", sdl_error.c_str());
+                break;
+            case RUNTIME_ERROR:
+                if (sdl_error != "") {
+                    SDL_Log("SDL runtime error: %s\n", sdl_error.c_str());
+                } else {
+                    SDL_Log("Runtime error: unknown cause\n");
+                }
+                break;
+            default:
+                SDL_Log("Undefined application status: %d\n", status);
+        }
+
+        return false;
     }
 
     /*
@@ -53,18 +74,74 @@ public:
         if ((scale = scale = SDL_GetDisplayContentScale(SDL_GetPrimaryDisplay())) == 0) {
             scale = 1;  // Use the scaling factor expected by the display based on its DPI settings, default to 1.
         }
-        SDL_Window* window = SDL_CreateWindow(window_title.c_str(), (int)(window_width * scale), (int)(window_height * scale), SDL_WINDOW_RESIZABLE);
+        SDL_WindowFlags flags = SDL_WINDOW_HIDDEN | SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_RESIZABLE;   // The window must be shown explicitly.
+        window = SDL_CreateWindow(window_title.c_str(), (int)(window_width * scale), (int)(window_height * scale), flags);
         if (window == nullptr) {
             status = INITIALIZATION_ERROR;
             return;
         }
 
         // Create an SDL renderer.
-        SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+        renderer = SDL_CreateRenderer(window, nullptr);
         if (renderer == nullptr) {
             status = INITIALIZATION_ERROR;
             return;
         }
+        // Synchronize renderer with each vertical refresh if possible.
+        if (SDL_SetRenderVSync(renderer, 1) == false) {
+            SDL_Log("SetRenderVSync: %s\n", SDL_GetError());
+        }
+
+        // Show window.
+        SDL_ShowWindow(window);
+    }
+
+    /*
+    Run the application.
+
+    @return The `status` value of the `Application` object is set by the function to reflect the state
+    of the application when the main loop has been terminated.
+    */
+    void run() {
+
+        // Load image to render and convert it to a texture.
+        SDL_Surface* bmp = SDL_LoadBMP("assets/uwu.bmp");
+        if (bmp == nullptr) {
+            status = RUNTIME_ERROR;
+            return;
+        }
+
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, bmp);
+        SDL_DestroySurface(bmp);
+        if (texture == nullptr) {
+            status = RUNTIME_ERROR;
+            return;
+        }
+
+        SDL_Event event;
+        SDL_zero(event);
+        bool quit = false;
+
+        // Enter the main loop.
+        while (quit == false) {
+            // Event handling.
+            while (SDL_PollEvent(&event)) {
+                // Close the window when necessary.
+                if (event.type == SDL_EVENT_QUIT) {
+                    quit = true;
+                }
+                if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(window)) {
+                    quit = true;
+                }
+            }
+
+            // Rendering logic.
+            SDL_RenderClear(renderer);
+            SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+            SDL_RenderPresent(renderer);
+        }
+
+        SDL_DestroyTexture(texture);
     }
 
     ~Application() {
@@ -92,17 +169,14 @@ public:
 int main() {
     Application application = Application(960, 540, "I am a window :3");
 
-    // Check the application's status before exit.
-    int status = application.get_status();
-    switch (status) {
-        case SUCCESS:
-            SDL_Log("Application successfully terminated.");
-            break;
-        case INITIALIZATION_ERROR:
-            SDL_Log("Initialization error: %s\n", SDL_GetError());
-            break;
-        default:
-            SDL_Log("Undefined application status: %d\n", status);
+    // Check for initialization errors.
+    bool status = application.print_status();
+    if (status == true) {
+        // Run the application.
+        application.run();
+
+        // Checking the return value is unnecessary since main() exits immediately afterwards.
+        application.print_status();
     }
 
     return status;
