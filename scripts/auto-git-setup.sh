84f16ac2c4a6519d2a692316ee6ba27b8a3bd125
Muhammed Reza Mehdi <muhammed.reza.mehdi@gmail.com>
2026-03-19 15:56:13 +0100

    chore/feat/fix: specify commits for all submodules, add new script and some functionality in the application (#58)
    * chore: set ImGui branch to docking.
    
    * docs: add git submodule commands to README.
    
    * test: ignore all submodule changes by default.
    
    * fix: remove trailing comma from CMakePresets.json and code formatting
    
    * chore: revert test.
    
    * chore: un-revert test changes.
    
    * feat: display dds-image after BC7 compression
    
    * chore: update .gitmodules to set large submodules to be shallow
    
    * chore: I did what Alfred told me to do.
    
    * chore: update specified commit for library
    
    * fix: created a script that sets external dependencies to correct releases and branches (meaning that imgui finally uses docking for anyone running the script after cloning the repository)
    
    * docs: update README.md with new instructions regarding submodules, urging the user to use the newly provided shell-script
    
    * docs: update README.md to have better instructions regarding the submoduleshell-script
    
    * docs/fix: update README.md to actually match reality, no longer encouraging developers to run a script that in most cases is not at all necessary. Also updated auto-git-setup.sh to no longer trying to init submodules, the user is now expected to do that themselves.
    
    ---------
    
    Co-authored-by: Lind <alfredlindgren@icloud.com>

---
+++ scripts/auto-git-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Checking out submodules to safe releases..."
+cd external
+cd SDL
+git fetch origin tag release-3.4.2 --no-tags
+git checkout tags/release-3.4.2 --force
+cd ..
+cd SDL_image
+git fetch origin tag release-3.4.0 --no-tags
+git checkout tags/release-3.4.0 --force
+cd ..
+cd bc7enc_rdo
+git fetch
+git checkout library
+cd ..
+cd imgui
+git fetch origin tag v1.92.6-docking --no-tags
+git checkout tags/v1.92.6-docking --force
+cd ..
+cd ..
+echo "Finished checking out submodules."
+
